d3cf4787c0260e5753361b46e65f48364435e297
Famondir <schaefersd@gmail.com>
2022-10-20 12:37:04 +0200

    searched for placec (Aufgabe 1a)

--- e3.R
+++ e3.R
@@ -1,10 +1,14 @@
-# library(tidyverse)
+library(tidyverse)
 library(DBI)
 # library(RSQLite)
 
 conn <- dbConnect(RSQLite::SQLite(), "aol.sqlite")
 
 dbGetQuery(conn, 'SELECT * FROM aol_data LIMIT 5')
-dbGetQuery(conn, 'SELECT * FROM aol_data WHERE ClickURL LIKE "%e3e%"')
+e3_website_visitors <- dbGetQuery(conn, 'SELECT * FROM aol_data WHERE ClickURL LIKE "%e3expo%"')
+
+temp_data <- dbGetQuery(conn, 'SELECT * FROM aol_data WHERE Query LIKE "%empire state building%"')
+t <- temp_data$Query %>% unique() %>% as_tibble()
+# 22748 könnte ein deutscher Tourist sein (sucht Deutsche Bank in New York)
 
 dbDisconnect(conn)
