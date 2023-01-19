11212a93d86b8bca61871dab90d670d788f753ff
Famondir <schaefersd@gmail.com>
2023-01-19 09:52:07 +0100

    data cleaning

--- gamesBombScraping.R
+++ gamesBombScraping.R
@@ -47,6 +47,10 @@ map_df_progress <- function(.x, .f, ..., .id = NULL) {
 # games_data %>% rio::export("crawled_games_data.xlsx")
 
 games_data <- rio::import("crawled_games_data.xlsx")
+games_data <- games_data %>% select(Name, `First release date`, Publisher, Platform, Aliases, rating, raters) %>% 
+  rowwise() %>%  mutate_at(vars("Name", "First release date"), ~str_split(.x, "\n")[[1]][1]) %>% 
+  mutate_at(vars("Name", "First release date"), ~str_split(.x, "\r")[[1]][1])
+games_data %>% rio::export("crawled_games_data.csv")
 
 sales_data <- rio::import("vgsales.csv") %>% mutate_if(is.numeric, ~.*1000000) %>% mutate(id = str_to_lower(Name))
 games <- games %>% mutate(id = str_to_lower(Names))
@@ -89,3 +93,55 @@ games[which(games$id == ""),1] <- ""
 games[which(games$id == ""),1] <- ""
 
 temp <- games %>% left_join(sales_data, by = c("id" = "id"))
+
+
+#### Publisher ####
+
+pub_names <- simple %>%
+  html_nodes("h3 a") %>%
+  html_text2()
+pub_urls <- simple %>%
+  html_nodes("h3 a") %>%
+  html_attr('href')
+
+publisher <- data.frame(Names = pub_names, url = pub_urls) %>% 
+  mutate(url = str_c("https://www.giantbomb.com", url))
+
+crawlData <- function(link) {
+  target_site <- tryCatch(
+    read_html(link),
+    error = function(e){NULL}
+  )
+  if (is.null(target_site)) return(NULL)
+  data <- target_site %>% html_nodes(".wiki-details .table:not(.js-release-table)") %>% html_table()
+  # starrating <- target_site %>% html_node(".average-score") %>% html_text2() %>% str_remove(" stars") %>% as.double()
+  # n_raters <- target_site %>% html_node(".average p") %>% html_text2() %>% str_extract_all("[0-9]*") %>% purrr::pluck(1) %>% str_flatten() %>% as.integer()
+  if (length(data)<1) return(NULL)
+  df <- data[[1]] %>% pivot_wider(names_from = "X1", values_from = "X2")
+}
+
+map_df_progress <- function(.x, .f, ..., .id = NULL) {
+  .f <- purrr::as_mapper(.f, ...)
+  pb <- progress::progress_bar$new(total = length(.x), force = TRUE)
+  
+  f <- function(...) {
+    pb$tick()
+    .f(...)
+  }
+  purrr::map_df(.x, f, ..., .id = .id)
+}
+
+pub_data <- map_df_progress(publisher$url, crawlData)
+pub_data <- pub_data %>% select(`Company Name`, Website) %>% 
+  rowwise() %>%  mutate_all(~str_split(.x, "\n")[[1]][1]) %>% 
+  mutate_all(~str_split(.x, "\r")[[1]][1])
+pub_data %>% rio::export("publisher_scrape.csv")
+
+# todo: ersetze website durch website id und erstelle website table
+
+#### Publisher-Games ####
+
+games_publisher_relation <- games_data %>% select(Name, Publisher) %>% 
+  separate_rows(Publisher, sep = "\r\r\n") %>% 
+  mutate(Publisher = str_remove(Publisher, "[ ]*")) %>% 
+  filter(Publisher != "", Publisher != "PublisherPublisher of this game.")
