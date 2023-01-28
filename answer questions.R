d42e70efba6d75838a53ac7423d866fc26ac2159
Famondir <schaefersd@gmail.com>
2023-01-28 18:00:15 +0100

    Question 1 and 2 in presentation

--- answer questions.R
+++ answer questions.R
@@ -3,7 +3,8 @@ library(DBI)
 
 searchinlist <- function(prestatement, list, poststatement) {
   last <- last(list)
-  list <- list[-length(list)]
+  # list <- list[-length(list)]
+  length(list) <- length(list)-1
   column <- last(str_split(prestatement, " ")[[1]])
   searchterms <- ""
   
@@ -27,10 +28,10 @@ query <- searchinlist("SELECT * FROM aol_data where query",
                       "group by anonid, querytime, clickurl;")
 q1_data_a <- dbGetQuery(conn, query) %>% as_tibble()
 
-# query <- searchinlist("SELECT * FROM aol_data where query", 
-#                       c('e3', 'e3 %', '% e3', '% e3 %'),
-#                       "group by anonid, querytime;")
-# q1_data_a_queries <- dbGetQuery(conn, query) %>% as_tibble()
+query <- searchinlist("SELECT * FROM aol_data where query",
+                      c('e3', 'e3 %', '% e3', '% e3 %'),
+                      "group by anonid, querytime;")
+q1_data_a_queries <- dbGetQuery(conn, query) %>% as_tibble()
 
 query <- searchinlist("SELECT count(distinct anonid) FROM aol_data where query", 
                       c('e3', 'e3 %', '% e3', '% e3 %'),
@@ -48,6 +49,16 @@ query <- searchinlist("SELECT count(distinct anonid) FROM aol_data where clickur
                       c('%e3expo.com%', '%e3insider.com%'),
                       "")
 anzahl_nutzer_e3_click <- dbGetQuery(conn, query) %>% pull()
+
+#### Q2 ####
+
+union(q1_data_b, q1_data_a) %>% ggplot() + 
+  geom_histogram(aes(x = QueryTime), color = "black") +
+  # geom_vline(xintercept = as.POSIXct("2006-05-10"), color = "#88FF55") +
+  # geom_vline(xintercept = as.POSIXct("2006-05-12"), color = "#88FF55") +
+  annotate("rect", xmin = as.POSIXct("2006-05-10"), xmax = as.POSIXct("2006-05-12"), 
+                ymin = -10, ymax = 120, fill = "#88FF55", alpha = .4) +
+  coord_cartesian(ylim = c(0, 110))
 
 #### Close connection ####
 dbDisconnect(conn)
